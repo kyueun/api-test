5e66434bcf48513ce5ff158c4611db3eecfe78e5
kyueun <kelee0812.2@gmail.com>
2021-05-07 13:23:00 +0900

    func: test query_search

--- elasticsearch/test.py
+++ elasticsearch/test.py
@@ -6,28 +6,35 @@ http = urllib3.PoolManager()
 
 class ESTest(unittest.TestCase):
     def test_query_search(self):
-        assert()
+        search = '문의'
+        res = http.request(
+            'GET',
+            'http://localhost:9200/test/_search?q=title:'+search
+        )
+
+        for doc in json.loads(res.data.decode('utf-8'))['hits']['hits']:
+            assert(doc['_source']['title'].find(search))
 
     def test_dsl_search(self):
         assert()
     
     def test_template(self):
         assert()
-        
+
     def test_search_template(self):
         assert()
 
-    def test_multisearch(self):
-        assert()
+    # def test_multisearch(self):
+    #     assert()
 
-    def test_multisearch_template(self):
-        assert()
+    # def test_multisearch_template(self):
+    #     assert()
 
-    def test_termvector(self):
-        assert()
+    # def test_termvector(self):
+    #     assert()
 
-    def test_multitermvector(self):
-        assert()
+    # def test_multitermvector(self):
+    #     assert()
 
 
 if __name__=='__main__':
